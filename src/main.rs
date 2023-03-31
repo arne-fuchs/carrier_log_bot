50760203e878d8e5ad15e0c2410de1cfcd104647
arne-fuchs <arnefuchs@protonmail.com>
2023-03-31 02:12:39 +0200

    Removed unused imports

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod journal_reader;
 fn main() {
     dotenv().expect(".env file not found");
     let discord = Discord::from_bot_token(std::env::var("BOT_TOKEN").unwrap().as_str()).expect("Login failed");
-    let (mut connection, _) = discord.connect().expect("Connect failed");
+    let (_connection, _) = discord.connect().expect("Connect failed");
     let channel = ChannelId(std::env::var("CHANNEL_ID").unwrap().parse().unwrap());
     let mut journal_reader = journal_reader::initialize(std::env::var("JOURNAL_PATH").unwrap(),discord,channel);
     println!("Ready");
