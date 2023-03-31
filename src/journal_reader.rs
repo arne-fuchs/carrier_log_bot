b80e50161ab7bd8960c710a25b7b2daaf4a202fa
arne-fuchs <arnefuchs@protonmail.com>
2023-03-31 19:59:52 +0200

    Added status and fixed racing condition, which may cause the bot to crash

--- src/journal_reader.rs
+++ src/journal_reader.rs
@@ -2,8 +2,9 @@ use std::fmt::format;
 use std::fs;
 use std::fs::{DirEntry, File};
 use std::io::{BufRead, BufReader};
+use std::ops::Add;
 use chrono::{NaiveDate, NaiveDateTime};
-use discord::Discord;
+use discord::{Connection, Discord};
 use discord::model::ChannelId;
 use json::JsonValue;
 
@@ -11,15 +12,17 @@ pub struct JournalReader{
     pub reader: BufReader<File>,
     directory_path: String,
     discord: Discord,
+    connection: Connection,
     channel: ChannelId
 }
 
-pub fn initialize(directory_path: String,discord: Discord, channel: ChannelId) -> JournalReader{
+pub fn initialize(directory_path: String,discord: Discord, connection: Connection, channel: ChannelId) -> JournalReader{
     let reader = get_journal_log_by_index(directory_path.clone(),0);
     JournalReader{
         reader,
         directory_path,
         discord,
+        connection,
         channel
     }
 }
@@ -35,33 +38,41 @@ impl JournalReader {
                     //Reached EOF -> does not mean new data wont come in
                 }else {
                     if !line.eq("") {
-                        //TODO Here logic what do write to discord
-                        let json = json::parse(&line).unwrap();
-                        let event = json["event"].as_str().unwrap();
-                        match event {
-                            //{ "timestamp":"2022-10-13T10:01:35Z", "event":"CarrierStats", "CarrierID":3704402432, "Callsign":"Q2K-BHB", "Name":"FUXBAU", "DockingAccess":"squadron", "AllowNotorious":true, "FuelLevel":617, "JumpRangeCurr":500.000000, "JumpRangeMax":500.000000, "PendingDecommission":false, "SpaceUsage":{ "TotalCapacity":25000, "Crew":6170, "Cargo":9331, "CargoSpaceReserved":1272, "ShipPacks":0, "ModulePacks":433, "FreeSpace":7794 }, "Finance":{ "CarrierBalance":1184935299, "ReserveBalance":51460958, "AvailableBalance":1029659181, "ReservePercent":4, "TaxRate_shipyard":15, "TaxRate_rearm":100, "TaxRate_outfitting":15, "TaxRate_refuel":100, "TaxRate_repair":100 }, "Crew":[ { "CrewRole":"BlackMarket", "Activated":false }, { "CrewRole":"Captain", "Activated":true, "Enabled":true, "CrewName":"Vada Cannon" }, { "CrewRole":"Refuel", "Activated":true, "Enabled":true, "CrewName":"Donna Moon" }, { "CrewRole":"Repair", "Activated":true, "Enabled":true, "CrewName":"Darnell Grant" }, { "CrewRole":"Rearm", "Activated":true, "Enabled":true, "CrewName":"Eiza York" }, { "CrewRole":"Commodities", "Activated":true, "Enabled":true, "CrewName":"Jewel King" }, { "CrewRole":"VoucherRedemption", "Activated":true, "Enabled":true, "CrewName":"Ezra Ramirez" }, { "CrewRole":"Exploration", "Activated":true, "Enabled":true, "CrewName":"Kasey Callahan" }, { "CrewRole":"Shipyard", "Activated":true, "Enabled":true, "CrewName":"Abby Cooke" }, { "CrewRole":"Outfitting", "Activated":true, "Enabled":true, "CrewName":"Jayne Callahan" }, { "CrewRole":"CarrierFuel", "Activated":true, "Enabled":true, "CrewName":"Abraham Strickland" }, { "CrewRole":"VistaGenomics", "Activated":true, "Enabled":true, "CrewName":"Melinda Reilly" }, { "CrewRole":"PioneerSupplies", "Activated":false }, { "CrewRole":"Bartender", "Activated":true, "Enabled":true, "CrewName":"Dean Barlow" } ], "ShipPacks":[  ], "ModulePacks":[ { "PackTheme":"VehicleSupport", "PackTier":1 }, { "PackTheme":"Storage", "PackTier":2 } ] }
-                            "CarrierStats" => {}
-                            //{ "timestamp":"2022-11-29T21:09:30Z", "event":"CarrierJumpRequest", "CarrierID":3704402432, "SystemName":"Ngorowai", "Body":"Ngorowai A", "SystemAddress":4207155286722, "BodyID":1, "DepartureTime":"2022-11-29T21:24:40Z" }
-                            "CarrierJumpRequest" => {
-                                println!("CarrierJumpRequest: {}",line);
-                                let text = format!("__**JUMP INITIATED**__\nDestination: {}\nBody: {}\nDeparture: {}",json["SystemName"],json["Body"],json["DepartureTime"]);
-                                self.discord.send_message(self.channel,text.as_str(),"",false).unwrap();
+                        let json_result = json::parse(&line);
+                        match json_result {
+                            Ok(json) => {
+                                let event = json["event"].as_str().unwrap();
+                                match event {
+                                    //{ "timestamp":"2022-10-13T10:01:35Z", "event":"CarrierStats", "CarrierID":3704402432, "Callsign":"Q2K-BHB", "Name":"FUXBAU", "DockingAccess":"squadron", "AllowNotorious":true, "FuelLevel":617, "JumpRangeCurr":500.000000, "JumpRangeMax":500.000000, "PendingDecommission":false, "SpaceUsage":{ "TotalCapacity":25000, "Crew":6170, "Cargo":9331, "CargoSpaceReserved":1272, "ShipPacks":0, "ModulePacks":433, "FreeSpace":7794 }, "Finance":{ "CarrierBalance":1184935299, "ReserveBalance":51460958, "AvailableBalance":1029659181, "ReservePercent":4, "TaxRate_shipyard":15, "TaxRate_rearm":100, "TaxRate_outfitting":15, "TaxRate_refuel":100, "TaxRate_repair":100 }, "Crew":[ { "CrewRole":"BlackMarket", "Activated":false }, { "CrewRole":"Captain", "Activated":true, "Enabled":true, "CrewName":"Vada Cannon" }, { "CrewRole":"Refuel", "Activated":true, "Enabled":true, "CrewName":"Donna Moon" }, { "CrewRole":"Repair", "Activated":true, "Enabled":true, "CrewName":"Darnell Grant" }, { "CrewRole":"Rearm", "Activated":true, "Enabled":true, "CrewName":"Eiza York" }, { "CrewRole":"Commodities", "Activated":true, "Enabled":true, "CrewName":"Jewel King" }, { "CrewRole":"VoucherRedemption", "Activated":true, "Enabled":true, "CrewName":"Ezra Ramirez" }, { "CrewRole":"Exploration", "Activated":true, "Enabled":true, "CrewName":"Kasey Callahan" }, { "CrewRole":"Shipyard", "Activated":true, "Enabled":true, "CrewName":"Abby Cooke" }, { "CrewRole":"Outfitting", "Activated":true, "Enabled":true, "CrewName":"Jayne Callahan" }, { "CrewRole":"CarrierFuel", "Activated":true, "Enabled":true, "CrewName":"Abraham Strickland" }, { "CrewRole":"VistaGenomics", "Activated":true, "Enabled":true, "CrewName":"Melinda Reilly" }, { "CrewRole":"PioneerSupplies", "Activated":false }, { "CrewRole":"Bartender", "Activated":true, "Enabled":true, "CrewName":"Dean Barlow" } ], "ShipPacks":[  ], "ModulePacks":[ { "PackTheme":"VehicleSupport", "PackTier":1 }, { "PackTheme":"Storage", "PackTier":2 } ] }
+                                    "CarrierStats" => {
+                                        println!("CarrierStats: {}",line);
+                                        let name = json["Name"].to_string().add(" ").add(json["Callsign"].as_str().unwrap());
+                                        self.connection.set_game_name(name);
+                                    }
+                                    //{ "timestamp":"2022-11-29T21:09:30Z", "event":"CarrierJumpRequest", "CarrierID":3704402432, "SystemName":"Ngorowai", "Body":"Ngorowai A", "SystemAddress":4207155286722, "BodyID":1, "DepartureTime":"2022-11-29T21:24:40Z" }
+                                    "CarrierJumpRequest" => {
+                                        println!("CarrierJumpRequest: {}",line);
+                                        let text = format!("__**JUMP INITIATED**__\nDestination: {}\nBody: {}\nDeparture: {}",json["SystemName"],json["Body"],json["DepartureTime"]);
+                                        self.discord.send_message(self.channel,text.as_str(),"",false).unwrap();
+                                    }
+                                    "CarrierTradeOrder" => {}
+                                    "CarrierFinance" => {}
+                                    //{ "timestamp":"2022-08-19T17:15:07Z", "event":"CarrierJumpCancelled", "CarrierID":3704402432 }
+                                    "CarrierJumpCancelled" => {
+                                        println!("CarrierJumpCancelled: {}",line);
+                                        let text = format!("__**JUMP CANCELED**__");
+                                        self.discord.send_message(self.channel,text.as_str(),"",false).unwrap();
+                                    }
+                                    "CarrierDepositFuel" => {}
+                                    "CarrierDockingPermission" => {}
+                                    "CarrierCrewServices" => {}
+                                    "Shutdown" => {
+                                        std::process::exit(0);
+                                    }
+                                    _ => {}
+                                }
                             }
-                            "CarrierTradeOrder" => {}
-                            "CarrierFinance" => {}
-                            //{ "timestamp":"2022-08-19T17:15:07Z", "event":"CarrierJumpCancelled", "CarrierID":3704402432 }
-                            "CarrierJumpCancelled" => {
-                                println!("CarrierJumpCancelled: {}",line);
-                                let text = format!("__**JUMP CANCELED**__");
-                                self.discord.send_message(self.channel,text.as_str(),"",false).unwrap();
-                            }
-                            "CarrierDepositFuel" => {}
-                            "CarrierDockingPermission" => {}
-                            "CarrierCrewServices" => {}
-                            "Shutdown" => {
-                                std::process::exit(0);
-                            }
-                            _ => {}
+                            Err(_) => {}
                         }
                     }
                 }
@@ -102,6 +113,8 @@ fn get_journal_log_by_index(mut directory_path: String, index: usize) -> BufRead
     directory_path.push_str("/Journal.");
     directory_path.push_str(log_name_date_list[index].to_owned().as_str());
     directory_path.push_str(".01.log");
+
+    println!("Opening journal: {}",directory_path);
 
     let journal_log_file = File::open(&directory_path).expect("file not found!");
 
--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ mod journal_reader;
 fn main() {
     dotenv().expect(".env file not found");
     let discord = Discord::from_bot_token(std::env::var("BOT_TOKEN").unwrap().as_str()).expect("Login failed");
-    let (_connection, _) = discord.connect().expect("Connect failed");
+    let (mut connection, _) = discord.connect().expect("Connect failed");
+    connection.set_game_name("Cruising in the void".to_string());
     let channel = ChannelId(std::env::var("CHANNEL_ID").unwrap().parse().unwrap());
-    let mut journal_reader = journal_reader::initialize(std::env::var("JOURNAL_PATH").unwrap(),discord,channel);
+    let mut journal_reader = journal_reader::initialize(std::env::var("JOURNAL_PATH").unwrap(),discord,connection,channel);
     println!("Ready");
     loop {
+        sleep(Duration::from_secs(1));
         journal_reader.run();
+        //let _ = connection.recv_event();
     }
 }
